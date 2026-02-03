9dd19ce0bfd229368a6d4921a5e55d8ee93cfb0a
unknown <y.ekayee48@gmail.com>
2026-02-03 16:17:27 +0700

    fix next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/verify1', destination: '/verify1/tiktokXJPIWZEecfGbl6gkwHdHijnUsvC58Mrq.txt' },
+      { source: '/verify1/', destination: '/verify1/tiktokXJPIWZEecfGbl6gkwHdHijnUsvC58Mrq.txt' },
+      { source: '/verify2', destination: '/verify2/tiktokVPaKtfErufTWFfbHGYworLvhcU9hGKHz.txt' },
+      { source: '/verify2/', destination: '/verify2/tiktokVPaKtfErufTWFfbHGYworLvhcU9hGKHz.txt' },
+      { source: '/verify3', destination: '/verify3/tiktokmN4artWeTJIC14VzZqY9K1rR5iFTyIFU.txt' },
+      { source: '/verify3/', destination: '/verify3/tiktokmN4artWeTJIC14VzZqY9K1rR5iFTyIFU.txt' },
+    ];
+  },
+};
 
 export default nextConfig;
